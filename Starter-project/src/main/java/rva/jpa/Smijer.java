0a460de6ea596a718a209b8425b5593a1216fc51
Milos <Milos@Milos-PC>
2021-03-22 09:32:38 +0100

    JPA klase

---
+++ Starter-project/src/main/java/rva/jpa/Smijer.java
@@ -0,0 +1,83 @@
+package rva.jpa;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+
+/**
+ * The persistent class for the smijer database table.
+ * 
+ */
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+@NamedQuery(name="Smijer.findAll", query="SELECT s FROM Smijer s")
+public class Smijer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@SequenceGenerator(name="SMIJER_ID_GENERATOR", sequenceName="SMIJER_SEQ")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SMIJER_ID_GENERATOR")
+	private Integer id;
+
+	private String naziv;
+
+	private String oznaka;
+
+	//bi-directional many-to-one association to Grupa
+	@OneToMany(mappedBy="smijer")
+	private List<Grupa> grupas;
+
+	public Smijer() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNaziv() {
+		return this.naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getOznaka() {
+		return this.oznaka;
+	}
+
+	public void setOznaka(String oznaka) {
+		this.oznaka = oznaka;
+	}
+
+	public List<Grupa> getGrupas() {
+		return this.grupas;
+	}
+
+	public void setGrupas(List<Grupa> grupas) {
+		this.grupas = grupas;
+	}
+
+	public Grupa addGrupa(Grupa grupa) {
+		getGrupas().add(grupa);
+		grupa.setSmijer(this);
+
+		return grupa;
+	}
+
+	public Grupa removeGrupa(Grupa grupa) {
+		getGrupas().remove(grupa);
+		grupa.setSmijer(null);
+
+		return grupa;
+	}
+
+}
---
+++ Starter-project/src/main/java/rva/repository/GrupaRepository.java
@@ -0,0 +1,13 @@
+package rva.repository;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import rva.jpa.Grupa;
+@Repository
+public interface GrupaRepository extends JpaRepository<Grupa, Integer>{
+
+	Collection<Grupa> findByOznakaContainingIgnoreCase(String oznaka);
+}
